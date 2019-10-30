a2a4d363fb50155292e8fd4d8717baf89c6f9f5d
tomlezen <tomlezen@protonmail.com>
2019-10-30 18:52:45 +0800

    新增room数据库支持

---
+++ lib/src/main/kotlin/com/tlz/ada/db/InMemorySQLiteDb.kt
@@ -0,0 +1,9 @@
+package com.tlz.ada.db
+
+/**
+ * Author: tomlezen
+ * Email: tomlezen@protonmail.com
+ * Date: 2019-10-30 13:31
+ */
+class InMemorySQLiteDb(val dName: String): SQLiteDb {
+}
---
+++ lib/src/main/kotlin/com/tlz/ada/db/SQLiteDb.kt
@@ -0,0 +1,31 @@
+package com.tlz.ada.db
+
+import android.content.ContentValues
+import android.database.Cursor
+import android.database.SQLException
+
+
+/**
+ * Created by Tomlezen.
+ * Date: 2019-10-28.
+ * Time: 21:53.
+ */
+interface SQLiteDb {
+
+    val isOpen: Boolean
+
+    val version: Boolean
+
+    fun delete(table: String, whereClause: String, whereArgs: Array<String>): Int
+
+    fun close()
+
+    fun rawQuery(sql: String, selectionArgs: Array<String>): Cursor
+
+    @Throws(SQLException::class)
+    fun execSQL(sql: String)
+
+    fun insert(table: String, nullColumnHack: String, values: ContentValues): Long
+
+    fun update(table: String, values: ContentValues, whereClause: String, whereArgs: Array<String>): Int
+}
